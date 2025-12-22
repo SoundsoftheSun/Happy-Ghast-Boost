4d24135d1e0756c93fe6881ecec7faceb39dc609
sun <sun@bluptr.xyz>
2025-12-22 18:55:02 -0500

    replaced custom effect with speed effect mixin

--- src/main/java/net/soundsofthesun/happyGhastBoost/mixin/FireChargeItemMixin.java
+++ src/main/java/net/soundsofthesun/happyGhastBoost/mixin/FireChargeItemMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.happyghast.HappyGhast;
 import net.minecraft.world.entity.player.Player;
@@ -15,7 +16,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ProjectileItem;
 import net.soundsofthesun.happyGhastBoost.attachments.MAttachments;
 import net.soundsofthesun.happyGhastBoost.attachments.SpeedBoostDuration;
-import net.soundsofthesun.happyGhastBoost.effects.MEffects;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 
@@ -31,7 +31,7 @@ public abstract class FireChargeItemMixin extends Item implements ProjectileItem
     public @NotNull InteractionResult interactLivingEntity(ItemStack stack, Player player, LivingEntity entity, InteractionHand usedHand) {
         if (entity.level() instanceof ServerLevel level && entity instanceof HappyGhast ghast) {
             if (!ghast.isBaby()) {
-                ghast.addEffect(new MobEffectInstance(MEffects.FLYING_SPEED_EFFECT, 20 * (level.getAttachedOrElse(MAttachments.SPEED_BOOST_DURATION, SpeedBoostDuration.DEFAULT)).seconds(), 1));
+                ghast.addEffect(new MobEffectInstance(MobEffects.SPEED, 20 * (level.getAttachedOrElse(MAttachments.SPEED_BOOST_DURATION, SpeedBoostDuration.DEFAULT)).seconds(), 1));
                 level.playSound(ghast, ghast, SoundEvents.GHAST_SHOOT, SoundSource.NEUTRAL, 1F, 1F);
                 stack.consume(1, player);
                 return InteractionResult.CONSUME;
---
+++ src/main/java/net/soundsofthesun/happyGhastBoost/mixin/MobEffectsMixin.java
@@ -0,0 +1,29 @@
+package net.soundsofthesun.happyGhastBoost.mixin;
+
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+import java.util.Objects;
+
+@Mixin(MobEffects.class)
+public class MobEffectsMixin {
+
+    @ModifyArgs(method = "register", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/Registry;registerForHolder(Lnet/minecraft/core/Registry;Lnet/minecraft/resources/Identifier;Ljava/lang/Object;)Lnet/minecraft/core/Holder$Reference;"))
+    private static void a(Args args) {
+        if (Objects.equals(args.get(1), Identifier.withDefaultNamespace("speed"))) {
+            args.set(2,
+                    new MobEffect(MobEffectCategory.BENEFICIAL, 3402751)
+                            .addAttributeModifier(Attributes.MOVEMENT_SPEED, Identifier.withDefaultNamespace("effect.speed"), 0.2F, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL)
+                            .addAttributeModifier(Attributes.FLYING_SPEED, Identifier.withDefaultNamespace("effect.speed"), 0.2F, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL)
+                    );
+        }
+    }
+}
